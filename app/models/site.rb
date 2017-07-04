cb6ac0bc179dd6fc9ac5a8bdf17530b1f53818c0
julietavuan <julietavuan@gmail.com>
2017-07-04 18:08:27 -0300

    Deleted unused code

--- app/models/site.rb
+++
@@ -1,5 +0,0 @@
-class Site < ActiveRecord::Base
-  belongs_to :parser
-
-  scope :by_host, ->(url) { where(url: url) }
-end
--- test/controllers/prueba_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PruebaControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get prueba_index_url
-    assert_response :success
-  end
-
-end
